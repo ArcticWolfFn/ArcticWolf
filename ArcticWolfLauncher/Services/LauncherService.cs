66ba6fa747486b6b5c77bafd29946beab996cf70
Joschua H <joschua.hass.sh@gmail.com>
2021-10-15 19:00:22 +0200

    ssl issues

--- ArcticWolfLauncher/Services/LauncherService.cs
+++ ArcticWolfLauncher/Services/LauncherService.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace ArcticWolfLauncher.Services
 {
@@ -53,6 +54,27 @@ namespace ArcticWolfLauncher.Services
                 CreateNoWindow = true,
                 FileName = "Resources/FortniteClient-Win64-Shipping_BE.exe"
             });
+
+            InjectDll(fortniteProcess.Id, "Resources/PlataniumV2.dll");
+        }
+
+        public static void InjectDll(int processId, string path)
+        {
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("DLL not found");
+                return;
+            }
+            var handle = Win32.OpenProcess(2 | 0x0400 | 8 | 0x0020 | 0x0010, false, processId);
+
+            var loadLibrary = Win32.GetProcAddress(Win32.GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+            var size = (uint)((path.Length + 1) * Marshal.SizeOf(typeof(char)));
+            var address = Win32.VirtualAllocEx(handle, IntPtr.Zero, size, 0x1000 | 0x2000, 4);
+
+            Win32.WriteProcessMemory(handle, address, Encoding.Default.GetBytes(path), size, out UIntPtr bytesWritten);
+
+            Win32.CreateRemoteThread(handle, IntPtr.Zero, 0, loadLibrary, address, 0, IntPtr.Zero);
         }
     }
 }
