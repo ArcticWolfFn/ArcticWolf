c8f4c27a2b9fd33ff32b1c5adff5edb142ca5aeb
Joschua H <joschua.hass.sh@gmail.com>
2021-12-21 12:29:55 +0100

    fixed wrong entry reason

--- ArcticWolf.Core/dllmain.cpp
+++ ArcticWolf.Core/dllmain.cpp
@@ -44,11 +44,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Main, hModule, 0, 0);
+		break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
-		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Main, hModule, 0, 0);
-        break;
+	default:
+		break;
     }
     return TRUE;
 }
