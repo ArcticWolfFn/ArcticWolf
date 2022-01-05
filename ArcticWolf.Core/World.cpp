59d3fcc779c42f315a75f6df96a102911737c54f
Joschua H <joschua.hass.sh@gmail.com>
2022-01-05 10:01:19 +0100

    removed world debug log

--- ArcticWolf.Core/World.cpp
+++ ArcticWolf.Core/World.cpp
@@ -43,6 +43,4 @@ void UWorld::UpdateProps()
 
     AuthorityGameMode = new AGameMode(WorldFinder.Find(L"AuthorityGameMode").GetObj());
     AuthorityGameMode->Setup();
-
-    PLOGV.printf("World is %ws", UE4::GetObjectName(WorldFinder.GetObj()));
 }
